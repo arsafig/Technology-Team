f54d757e806d981942bc12dfb70e09e003191ea2
arsafig <arsafig@fmail.com>
2021-01-20 16:41:43 -0400

    Creacion de proyecto base + componente de Login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,43 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { DashboardComponent } from './paginas/dashboard/dashboard.component';
+import { SidebarComponent } from './componentes/sidebar/sidebar.component';
+import { NavbarComponent } from './componentes/navbar/navbar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// formularios reactivos
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+
+//Angular material
+import {MatInputModule} from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import {MatButtonModule} from '@angular/material/button';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DashboardComponent,
+    SidebarComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule, 
+    MatInputModule, 
+    MatIconModule, 
+    MatFormFieldModule, 
+    MatCheckboxModule, 
+    MatButtonModule,
+    FormsModule, 
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/componentes/login/login.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  oculto: boolean = true;
+  usuario!: FormGroup;
+
+  constructor(
+    private _formBuilder: FormBuilder,
+    private _router: Router,
+  ) { }
+
+  ngOnInit(): void {
+    this.formularioUsuario();
+  }
+
+  formularioUsuario() {
+    this.usuario = this._formBuilder.group({
+      usuario: ['', [Validators.required, Validators.minLength(4), Validators.pattern('^[a-z]*$')]],
+      password: ['', [Validators.required, Validators.minLength(4),Validators.pattern('^[A-Z0-9~!@#$%^&*()_+=-]*$')]],
+      privacidad: ['', [Validators.required]],
+
+    })
+  }
+
+  validar(control: string): boolean {
+
+    return (this.usuario.get(control).invalid && this.usuario.get(control).touched);
+  }
+
+
+  login() {
+    if(this.usuario.valid){
+      this._router.navigateByUrl('/dashboard');
+    }
+  }
+}
